027dd2c7b8625fb8b9cde53220ef6a4500a1c3b5
DeathKnightH <919438785@qq>
2019-12-25 17:32:11 +0800

    新增eureka server 和 config server

--- circuit-breaker-sample/src/main/java/cn/dk/cloud/breaker/resilience4j/RateLimiterSample.java
+++ circuit-breaker-sample/src/main/java/cn/dk/cloud/breaker/resilience4j/RateLimiterSample.java
@@ -13,9 +13,9 @@ public class RateLimiterSample {
 
     public RateLimiterSample() {
         RateLimiterConfig rateLimiterConfig = RateLimiterConfig.custom()
-                .timeoutDuration(Duration.ofSeconds(3)) //等待超时时间，默认5秒 设置为3秒
+                .timeoutDuration(Duration.ofSeconds(1)) //等待超时时间，默认5秒 设置为3秒
                 .limitForPeriod(2)  //一个限流周期内运行通过的 calls 数量，默认50个 设置为2
-                .limitRefreshPeriod(Duration.ofSeconds(1))  //限流周期，其实也是限制刷新周期，默认500纳秒 设置为1秒
+                .limitRefreshPeriod(Duration.ofSeconds(2))  //限流周期，其实也是限制刷新周期，默认500纳秒 设置为1秒
                 .build();
         this.rateLimiterRegistry = RateLimiterRegistry.of(rateLimiterConfig);
     }
